a3db73fe9901c33f40a5f851dbce13f5827767bc
euphoriaqn <euphoriaqn@gmail.com>
2017-09-01 19:00:55 +0300

    удален лишний код

--- src/HomeworkJson/TradeShop.java
+++ src/HomeworkJson/TradeShop.java
@@ -16,7 +16,6 @@ public class TradeShop {
     public List<Fruit> fruits = new ArrayList<>();
     public  void addFruits(String pathToJsonFile) throws IOException {
         String json = new Scanner(new File(pathToJsonFile)).useDelimiter("\\Z").next();
-        System.out.println("Loading data");
         TradeShop tradeShop = JSON.parseObject(json, TradeShop.class);
         for (Fruit value: tradeShop.fruits) {
             fruits.add(value);
@@ -32,7 +31,6 @@ public class TradeShop {
     }
     public void load(String pathToJsonFile) throws IOException {
         fruits.clear();
-        System.out.println("Loading data");
         String json = new Scanner(new File(pathToJsonFile)).useDelimiter("\\Z").next();
         TradeShop tradeShop = JSON.parseObject(json, TradeShop.class);
         for (Fruit value: tradeShop.fruits) {
